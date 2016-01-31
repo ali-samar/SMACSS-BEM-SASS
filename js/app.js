4f6b1598bc6d595bd899452ad8a7d6477c0ca4dc
Dant Benjamin <sukkhato@gmail.com>
2016-01-31 20:41:57 +0100

    Test de SASS

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(function(){
 /* Initialisation de senna */
 var app = new senna.App();
     app.setBasePath('/');
-    app.addSurfaces(['content', 'bandeau']);
+    app.addSurfaces(['content']);
     app.addRoutes(new senna.Route(/\w+\.html/, senna.HtmlScreen));
     senna.Screen.prototype.activate = function() {
 		$.getJSON('https://smp.ovh/ppl.json', function(data){
@@ -20,4 +20,24 @@ setInterval(function() {
 		$(this).css({marginLeft : 0});
 	});
 }, 5000);
+
+/* Twitter
+getTwitters('tweet', {
+	id: 'simplonMIP',
+	count: 5,
+	enableLinks: true,
+	ignoreReplies: true,
+	clearContents: true,
+	template: '%text% <a href="http://twitter.com/%user_screen_name%/statuses/%id%/">%time%</a>'
+});
+$.ajax({
+	url: 'https://api.twitter.com/1.1/search/tweets.json?q=from%3AsimplonMIP&result_type=popular',
+	type: 'GET',
+	dataType: 'json',
+	success: function(json){
+		console.log(json);
+	},
+	error: function(){console.log('erreur dans la requête twitter')}
+});*/
+
 });
